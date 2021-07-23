5e7030343f24150fabf721c3a5c053cf0b8c07cf
Rohan-hacker <83770475+Rohan-hacker@users.noreply.github.com>
2021-07-23 17:54:01 +0530

    Update stone.js

--- js/stone.js
+++ js/stone.js
@@ -12,7 +12,7 @@ class stone
 		this.y=y;
 		this.r=r
 		this.image=loadImage("images/stone.png");
-		this.image.scale=0.5;
+		//this.image.scale=0.5;
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
 		World.add(world, this.body);
 
